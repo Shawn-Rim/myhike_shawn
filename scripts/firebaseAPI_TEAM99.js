c6b1ae0f922fd052d52c422cac9bd4d9dd902fe4
Shawn Rim <trim2@my.bcit.ca>
2024-02-20 10:26:55 -0800

    Finish Demo07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAsmLDZkt1HDtEIXEqPMacwGLN1W-I-9MM",
+    authDomain: "comp1800-202410-demo-7d2b2.firebaseapp.com",
+    projectId: "comp1800-202410-demo-7d2b2",
+    storageBucket: "comp1800-202410-demo-7d2b2.appspot.com",
+    messagingSenderId: "174913190584",
+    appId: "1:174913190584:web:d5765d02cd2489038f524d",
 };
 
 //--------------------------------------------
